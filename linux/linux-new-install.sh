ea4c0c443b74be3dc138d18d91350f9c6970b93a
headintheclouds <93227818+mili-sucevic@users.noreply.github.com>
2023-01-24 14:32:34 +0100

    remove typo -e

--- linux/linux-new-install.sh
+++ linux/linux-new-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Update package index
-echo -e -e "\e[93mUpdate package index...\e[0m"
+echo -e "\e[93mUpdate package index...\e[0m"
 sudo apt-get update
 sudo apt-get install -y zsh
 echo
 
 # Install Terraform
-echo -e -e "\e[93mInstall Terraform and add to PATH...\e[0m"
+echo -e "\e[93mInstall Terraform and add to PATH...\e[0m"
 sudo snap install terraform --classic
 
 # Add Terraform to PATH
