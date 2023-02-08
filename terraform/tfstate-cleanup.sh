95a15393026bff0f92ba4bf6f41f0dc425e24a4a
headintheclouds <93227818+mili-sucevic@users.noreply.github.com>
2023-02-08 10:53:20 +0100

    Update tfstate-cleanup.sh

--- terraform/tfstate-cleanup.sh
+++ terraform/tfstate-cleanup.sh
@@ -4,7 +4,7 @@
 dir_to_search=$1
 
 # Find all terraform.tfstate files in the directory and its subdirectories and remove them
-find $dir_to_search -name "terraform.tfstate*" -delete
+find $dir_to_search -name "terraform.tfstate*" -or -name "terraform.tfstate.backup" -delete
 
-# Remove .terraform directory
-find . -name ".terraform" && find . -name ".terraform.lock.hcl" -exec rm -rf {} \;
+# Remove .terraform directory and its lock file
+find $dir_to_search -name ".terraform" -or -name ".terraform.lock.hcl" -exec rm -rf {} \;
